58a418f3599d2feb60382f43cc7e542d05b03340
Pervez Choudhury <codedemonuk@gmail.com>
2026-04-03 21:55:59 +0100

    Make this work with GitHub pages hosting

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import type { Config } from '@sveltejs/kit';
 
 const config: Config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		})
 	}
 };
 
